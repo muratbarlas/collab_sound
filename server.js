9352010a2c291c1574f1d8dbdeb33f8f44ab9efb
muratbarlas <47250734+muratbarlas@users.noreply.github.com>
2021-07-29 17:02:51 +0300

    all files

---
+++ server.js
@@ -0,0 +1,25 @@
+console.log("Server is running")
+
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000);
+
+app.use(express.static("public"));
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log("new connection " + socket.id);
+    socket.on('arrayUpdate', arrayMessage);
+
+    function arrayMessage(data){
+        //console.log(data);
+        socket.broadcast.emit('arrayUpdate', data); //sends data to all windows
+
+    }
+}
